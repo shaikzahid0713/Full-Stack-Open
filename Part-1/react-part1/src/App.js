bdcec09373c0d09c69538f29479fafa4055a1048
shaikzahid0713 <shaikzahid0713@gmail.com>
2023-03-04 15:50:38 +0530

    React Assignment 1.5
    Part-1 -> react-part1 -> src/App.js -> 'https://fullstackopen.com/en/part1/java_script#exercises-1-3-1-5' -> 1.5 -> Completed

--- Part-1/react-part1/src/App.js
+++ Part-1/react-part1/src/App.js
@@ -1,38 +1,40 @@
 // Using Objects to Modify the same application
-// Exercise - 1.4
-// 
+// Exercise - 1.5
+
 
 const App = () => {
-  const course = "Half Stack Application Development"
-  const parts = [
-    {
-      name : 'Fundamentals of React',
-      exercise: 10
-    },
-    {
-      name : 'Using props to pass data',
-      exercise: 7
-    },
-    {
-      name : 'State of a Component',
-      exercise: 14
-    }
-  ]
- 
+  const course = {
+    name : "Half Stack Application Development",
+    parts : [
+      {
+        name : 'Fundamentals of React',
+        exercise: 10
+      },
+      {
+        name : 'Using props to pass data',
+        exercise: 7
+      },
+      {
+        name : 'State of a Component',
+        exercise: 14
+      }
+    ]
+  }
+     
   return (
     <div>
-      <h1> {course} </h1>
+      <h1> {course.name} </h1>
       <p>
-        {parts[0].name} {parts[0].exercise}
+        {course.parts[0].name} {course.parts[0].exercise}
       </p>
       <p>
-        {parts[1].name} {parts[1].exercise}
+        {course.parts[1].name} {course.parts[1].exercise}
       </p>
       <p> 
-        {parts[2].name} {parts[2].exercise}
+        {course.parts[2].name} {course.parts[2].exercise}
       </p>
       <p>
-        Number of exercises are {parts[0].exercise + parts[1].exercise + parts[2].exercise}
+        Number of exercises are {course.parts[0].exercise + course.parts[1].exercise + course.parts[2].exercise}
       </p>
     </div>
   )
