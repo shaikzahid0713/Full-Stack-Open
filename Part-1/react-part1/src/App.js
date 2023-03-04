1417ddb593aa0d9f0f8386b2cf337c88532be5b8
shaikzahid0713 <shaikzahid0713@gmail.com>
2023-03-04 14:40:39 +0530

    React Assignment 1a
    Part-1 -> react-part1 -> App.js -> Half Stack Application Development -> 'https://fullstackopen.com/en/part1/introduction_to_react#exercises-1-1-1-2' -> Completed

---
+++ Part-1/react-part1/src/App.js
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <h1> {props.course} </h1>
+  )
+} 
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1} exercise = {props.exercise1} />
+      <Part part = {props.part2} exercise = {props.exercise2} />
+      <Part part = {props.part3} exercise = {props.exercise3} />
+    </div>
+  )
+} 
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises are {props.exercise1 + props.exercise2 + props.exercise3}
+    </p>
+  )
+}
+
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercise1 = 10
+  const part2 = "Using props to pass data"
+  const exercise2 = 7
+  const part3 = "State of a Component"
+  const exercise3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercise1 = {exercise1} exercise2 = {exercise2} exercise3 = {exercise3} />
+      <Total exercise1 = {exercise1} exercise2 = {exercise2} exercise3 = {exercise3} />
+    </div>
+  )
+}
+
+export default App
